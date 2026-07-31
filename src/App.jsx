2dcd56db0afcf54d3e84fc1627b329d84dd1e278
Giovanni D'Alessandro <giovanni.dalessandro@gruppoinfordata.lan>
2026-07-31 14:54:23 +0200

    Test bottone

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,14 @@
 import { useState } from "react";
 export default function App() {
   const [tableCode, setTableCode] = useState("");
-  function createTable() {
-    setTableCode("TEST1");
+function createTable() {
+  const chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+  let code = "";
+  for (let i = 0; i < 5; i++) {
+    code += chars.charAt(Math.floor(Math.random() * chars.length));
   }
+  setTableCode(code);
+}
   return (
     <div>
       <h1>WHIST ONLINE</h1> <button onClick={createTable}>CREA TAVOLO</button> 
