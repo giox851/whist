f55ccf849199be34d6bb19440ce775b7b71ceb52
Giovanni D'Alessandro <giovanni.dalessandro@gruppoinfordata.lan>
2026-07-31 14:52:20 +0200

    Test bottone

--- src/App.jsx
+++ src/App.jsx
@@ -1,43 +1,17 @@
 import { useState } from "react";
-import { collection, doc, setDoc } from "firebase/firestore";
-import { db } from "./services/firebase";
 export default function App() {
   const [tableCode, setTableCode] = useState("");
-  function generateCode() {
-    const chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
-    let code = "";
-    for (let i = 0; i < 5; i++) {
-      code += chars.charAt(Math.floor(Math.random() * chars.length));
-    }
-    return code;
-  }
-  async function createTable() {
-    const code = generateCode();
-    await setDoc(doc(collection(db, "tables"), code), {
-      tableId: code,
-      status: "waiting",
-      createdAt: new Date().toISOString(),
-      players: {},
-    });
-    setTableCode(code);
+  function createTable() {
+    setTableCode("TEST1");
   }
   return (
-    <div
-      style={{
-        display: "flex",
-        flexDirection: "column",
-        alignItems: "center",
-        justifyContent: "center",
-        minHeight: "100vh",
-        gap: "20px",
-      }}
-    >
+    <div>
       <h1>WHIST ONLINE</h1> <button onClick={createTable}>CREA TAVOLO</button> 
       {tableCode && (
-        <>
+        <div>
           <h2>Codice Tavolo</h2>
           <h1>{tableCode}</h1>
-        </>
+        </div>
       )}
     </div>
   );
